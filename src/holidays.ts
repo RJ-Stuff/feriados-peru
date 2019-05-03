e64288725148309951a24e4a6120b89d14993079
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2019-05-03 13:21:35 -0500

    updating models

--- src/holidays.ts
+++ src/holidays.ts
@@ -4,70 +4,84 @@
 export const holidays: any[] = [{
     "id": 1,
     "date": "2019-01-01",
+    "extra": "irrenunciable",
     "title": "Año Nuevo"
 },
 {
     "id": 2,
     "date": "2019-04-18",
+    "extra": "irrenunciable",
     "title": "Jueves Santo"
 },
 {
     "id": 3,
     "date": "2019-04-19",
+    "extra": "irrenunciable",
     "title": "Viernes Santo"
 },
 {
     "id": 4,
     "date": "2019-04-21",
+    "extra": "irrenunciable",
     "title": "Domingo de Resurrección"
 },
 {
     "id": 5,
     "date": "2019-05-01",
+    "extra": "irrenunciable",
     "title": "Día del Trabajo"
 },
 {
     "id": 6,
     "date": "2019-06-24",
+    "extra": "irrenunciable",
     "title": "Fiesta del Inti Raymi"
 },
 {
     "id": 7,
     "date": "2019-06-29",
+    "extra": "irrenunciable",
     "title": "San Pedro y San Pablo"
 },
 {
     "id": 8,
+    "extra": "irrenunciable",
     "date": "2019-07-28",
     "title": "Fiestas Patrias"
 },
 {
     "id": 9,
+    "extra": "irrenunciable",
     "date": "2019-07-29",
     "title": "Fiestas Patrias"
 },
 {
     "id": 10,
+    "extra": "irrenunciable",
     "date": "2019-08-30",
     "title": "Santa Rosa de Lima"
 },
 {
     "id": 11,
+    "extra": "irrenunciable",
     "date": "2019-10-08",
     "title": "Combate de Angamos"
 },
 {
     "id": 12,
+    "extra": "irrenunciable",
     "date": "2019-11-01",
     "title": "Todos los Santos"
 },
 {
     "id": 13,
+    "extra": "irrenunciable",
     "date": "2019-12-08",
     "title": "Inmaculada Concepción"
 },
 {
     "id": 14,
+    "extra": "irrenunciable",
     "date": "2019-12-25",
     "title": "Navidad"
 }
